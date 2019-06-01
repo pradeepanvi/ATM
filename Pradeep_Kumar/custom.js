7cf16b020f87982bc08ef8087cb6e655a3d655c7
pradeepanvi <pradeepkumar.anvi@gmail.com>
2019-06-01 15:08:03 +0530

    update js and HTML

--- Pradeep_Kumar/custom.js
+++ Pradeep_Kumar/custom.js
@@ -1,34 +1,28 @@
-var btn = document.querySelector('.btn');
+var form = document.querySelector('form');
 var inputV = document.querySelector('.form-control');
-
-//notes
-var notes = document.querySelectorAll('[class^=note_]')
 
 //total_notes
 var total_notes = document.querySelector('.total_notes');
 var notesArray = [2000,500,200,100,50,10,5,2,1];
+var finalArray = [];
+var total_notes_q = 0;
 
-btn.addEventListener('click', function(){
+form.onsubmit = function(){
     var inputVal = inputV.value;
     if(inputVal != ''){
         for(var i=0; i<notesArray.length; i++){
-            if(inputVal > notesArray[i]){
-                var notes = 0;
-                for(var x=0; x<notesArray[i]; x++){
-                    var myVal = inputVal - notesArray[i];
-                    document.querySelector('.note_'+notesArray[i]).innerHTML = parseInt(notes);
-                    notes++;
-                    inputVal = Math.abs(myVal);
-                }
-            }
+                finalArray.push(Math.floor(inputVal / notesArray[i]));
+                inputVal = inputVal % notesArray[i];
         }
-        for(var z=0; z<notesArray.length; z++){
-            total_notes.innerHTML + document.querySelector('.note_'+notesArray[z]).innerHTML;
-            total_notes.innerHTML;
+        for(var x=0; x<finalArray.length; x++){
+            document.querySelector('.note_'+notesArray[x]).innerHTML = finalArray[x];
+        }        
+        for(var z=0; z<finalArray.length; z++){
+            total_notes_q += finalArray[z];
         }
+        total_notes.innerHTML = total_notes_q;
     } else {
         alert('please enter value');
     }
-    console.log(inputVal);
-
-})    
+    return false;
+};
